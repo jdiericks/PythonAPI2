19bb47ff6a3ee3735cc627c09f2ad6254d5d4032
Jon-Luke <jdwebdesigns@live.com>
2022-07-08 16:27:07 -0500

    Update

--- app/db.py
+++ app/db.py
@@ -2,6 +2,7 @@ from enum import unique
 import databases
 import ormar
 import sqlalchemy
+from typing import Optional, List
 
 from .config import settings
 
@@ -18,7 +19,26 @@ class User(ormar.Model):
 
     id: int = ormar.Integer(primary_key=True)
     email: str = ormar.String(max_length=128, unique=True, nullable=False)
+    fname: str = ormar.String(max_length=128, nullable=True)
+    lname: str = ormar.String(max_length=128, nullable=True)
     active: bool = ormar.Boolean(default=True, nullable=False)
+
+class UserCreate(User):
+    password: str = ormar.String(max_length=128, nullable=False)
+
+class Report(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "reports"
+
+    id: int = ormar.Integer(primary_key=True)
+    user: User = ormar.ForeignKey(User, nullable=False, orders_by=["lname"], related_orders_by=["id"])
+    service_hours: int = ormar.Integer(nullable=False)
+    service_mintues: int = ormar.Integer(nullable=False)
+    placements: int = ormar.Integer(nullable=False)
+    videos: int = ormar.Integer(nullable=False)
+    return_visits: int = ormar.Integer(nullable=False)
+    bible_studies: int = ormar.Integer(nullable=False)
+    notes: str = ormar.String(max_length=256, nullable=False)
 
 engine = sqlalchemy.create_engine(settings.db_url)
 metadata.create_all(engine)
--- app/main.py
+++ app/main.py
@@ -1,20 +1,63 @@
 # app/main.py
+from http.client import HTTPException
 from fastapi import FastAPI
 
-from app.db import database, User
+from app.db import database, User, UserCreate, Report
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def auth_user(email, password):
+    user = User.get_by_email(email)
+    if user is None:
+        return None
+    if not verify_password(password, user.password):
+        return None
+    return user
 
 app = FastAPI(title="Service Reportr", description="Service Reportr API", version="0.1.0")
 
 @app.get("/")
 async def root():
-    return await User.objects.all()
+    return {"message": "Hello World"}
+
+@app.post("/user")
+async def create_user(user: UserCreate):
+    hashed_pass = get_password_hash(user.password)
+    user = User.get_by_email(user.email)
+    if user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    user = User(email=user.email, password=hashed_pass, fname=user.fname, lname=user.lname)
+    await user.save()
+    return user
+
+@app.get("/user")
+async def get_users():
+    return await User.objects.select_related(User.reports).filter(active=True).all()
+
+@app.post("/report")
+async def create_report(report: Report):
+    await report.save()
+    return report
+
+@app.get("/report")
+async def get_reports():
+    return await Report.objects.all()
 
 @app.on_event("startup")
 async def startup():
     if not database.is_connected:
         await database.connect()
     # create a dummy user
-    await User.objects.get_or_create(email="test@test.com")
+    await User.objects.get_or_create(email="test@test.com", fname="Test", lname="User")
+    await Report.objects.get_or_create(user=1, service_hours=10, service_mintues=15, placements=3, videos=0, return_visits=4, bible_studies=1, notes="Conducted a bible study with Jim in June")
 
 @app.on_event("shutdown")
 async def shutdown():
